d7a41b15207f7e978a528d28893e014b99266a0b
Jayson Goh Chung Sern <35084032+JaysonGCS@users.noreply.github.com>
2025-04-07 01:03:49 +0800

    fix: alias issue (#14)
    * fix: alias replacer issue

--- aliasReplacer.ts
+++ aliasReplacer.ts
@@ -1,10 +1,7 @@
 import { AliasReplacerArguments } from 'tsc-alias';
 
 export default function exampleReplacer({ orig, file, config }: AliasReplacerArguments): string {
-  if (
-    file === '/Users/jaysongcs/Projects/atomic-state-canvas/dist/apps/cli/src/index.js' &&
-    orig === 'require("@atomic-state-canvas/core")'
-  ) {
+  if (file.startsWith(config.outPath) && orig === 'require("@atomic-state-canvas/core")') {
     // FIXME: Replace with the path derived from the config dynamically
     return 'require("../../../libs/core/src/index")';
   }
