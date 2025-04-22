088d2d48d0507169e001caeef2b4bb136d47ebaa
“JaysonGCS“ <goh.chung.sern@gmail.com>
2025-04-22 12:17:00 +0800

    feat: add cyclic detection

--- apps/asc-viewer/src/app/canvas.tsx
+++ apps/asc-viewer/src/app/canvas.tsx
@@ -1,29 +1,49 @@
 import { useAtomValue } from 'jotai';
 import { useMemo } from 'react';
-import ForceGraph3D, { GraphData, NodeObject } from 'react-force-graph-3d';
+import ForceGraph3D, { GraphData, LinkObject, NodeObject } from 'react-force-graph-3d';
 import { currentAscEntryAtom } from '../stores/ascStore/ascStore';
+import { findAllCycles } from '@atomic-state-canvas/asc-viewer-libs';
+
+type TNodeExtra = { color: string };
+type TLinkExtra = { color: string };
 
 export const Canvas = () => {
   const ascEntry = useAtomValue(currentAscEntryAtom);
 
-  const graphData = useMemo<GraphData>(() => {
+  const graphData = useMemo<GraphData<TNodeExtra, TLinkExtra>>(() => {
     if (ascEntry) {
       const edges = ascEntry?.reverseEdges ?? [];
-      // TODO: Implement cycle detection
-      // const { allCycles, cyclicDetailsMap, cyclicStatsMap } = findAllCycles(edges);
-      const idToNodesMap = edges.reduce<Map<string, NodeObject>>((total, edge) => {
+      const { cyclicDetailsMap } = findAllCycles(edges);
+      const idToNodesMap = edges.reduce<Map<string, NodeObject<TNodeExtra>>>((total, edge) => {
         const { source, target } = edge;
         const sourceNode = total.get(source);
         const targetNode = total.get(target);
         if (!sourceNode) {
-          total.set(source, { id: source });
+          let color = 'green';
+          if (cyclicDetailsMap.has(source)) {
+            color = 'red';
+          }
+          total.set(source, { id: source, color });
         }
         if (!targetNode) {
-          total.set(target, { id: target });
+          let color = 'green';
+          if (cyclicDetailsMap.has(target)) {
+            color = 'red';
+          }
+          total.set(target, { id: target, color });
         }
         return total;
-      }, new Map<string, NodeObject>());
-      return { nodes: Array.from(idToNodesMap.values()), links: edges };
+      }, new Map<string, NodeObject<TNodeExtra>>());
+      const links = edges.map<LinkObject<TNodeExtra, TLinkExtra>>((edge) => {
+        return {
+          ...edge,
+          color:
+            cyclicDetailsMap.has(edge.source) && cyclicDetailsMap.has(edge.target)
+              ? 'yellow'
+              : 'teal'
+        };
+      });
+      return { nodes: Array.from(idToNodesMap.values()), links };
     }
     return { nodes: [], links: [] };
   }, [ascEntry]);
@@ -34,6 +54,14 @@ export const Canvas = () => {
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
       linkCurvature={0.25}
+      linkWidth={1}
+      nodeColor={(props: NodeObject<TNodeExtra>) => {
+        return props.color;
+      }}
+      linkColor={(props: LinkObject<TNodeExtra, TLinkExtra>) => {
+        return props.color;
+      }}
+      backgroundColor="white"
     />
   );
 };
