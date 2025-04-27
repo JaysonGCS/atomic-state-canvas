e7408347af4a0357dc5722b462edd308d4d3cddb
“JaysonGCS“ <goh.chung.sern@gmail.com>
2025-04-27 13:15:07 +0800

    feat: add 2d support

--- apps/asc-viewer/src/app/app.tsx
+++ apps/asc-viewer/src/app/app.tsx
@@ -2,10 +2,12 @@ import { SidebarInset, SidebarProvider } from '@atomic-state-canvas/components/u
 import { AppSideBar } from '@atomic-state-canvas/components/wrapper/AppSideBar';
 import { useAtomValue, useSetAtom } from 'jotai';
 import { loadable } from 'jotai/utils';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 import { ascHierarchyAtom, currentAscIdAtom } from '../stores/ascStore/ascStore';
 import { THierarchyItem } from '../stores/ascStore/types';
 import { Canvas } from './canvas';
+import { Tabs, TabsList, TabsTrigger } from '@atomic-state-canvas/components/ui/tabs';
+import { isDimensionValid, TViewDimension } from './utils';
 
 const loadableAscHierarchyAtom = loadable<Promise<THierarchyItem>>(ascHierarchyAtom);
 const loadableCurrentAscIdAtom = loadable<Promise<string | undefined>>(currentAscIdAtom);
@@ -14,6 +16,7 @@ export function App() {
   const ascHierarchyLoadable = useAtomValue(loadableAscHierarchyAtom);
   const currentAscIdLoadable = useAtomValue(loadableCurrentAscIdAtom);
   const setCurrentAscId = useSetAtom(currentAscIdAtom);
+  const [dimension, setDimension] = useState<TViewDimension>('2D');
 
   const handleItemClick = useCallback(
     (itemId: string) => {
@@ -21,6 +24,14 @@ export function App() {
     },
     [setCurrentAscId]
   );
+
+  const handleChangeDimension = useCallback((value: string) => {
+    if (isDimensionValid(value)) {
+      setDimension(value);
+    } else {
+      console.error(`Invalid dimension value: ${value}`);
+    }
+  }, []);
 
   return (
     <div>
@@ -36,7 +47,16 @@ export function App() {
           onItemClick={handleItemClick}
         />
         <SidebarInset className="bg-slate-50 bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]">
-          <Canvas />
+          <Canvas type={dimension} />
+          <Tabs
+            defaultValue={dimension}
+            onValueChange={handleChangeDimension}
+            className="absolute left-1/2 top-4 -translate-x-1/2">
+            <TabsList>
+              <TabsTrigger value={'2D' satisfies TViewDimension}>2 Dimensional</TabsTrigger>
+              <TabsTrigger value={'3D' satisfies TViewDimension}>3 Dimensional</TabsTrigger>
+            </TabsList>
+          </Tabs>
         </SidebarInset>
       </SidebarProvider>
     </div>
--- apps/asc-viewer/src/app/canvas.tsx
+++ apps/asc-viewer/src/app/canvas.tsx
@@ -9,6 +9,16 @@ import ForceGraph3D, {
 import { currentAscEntryAtom } from '../stores/ascStore/ascStore';
 import { findAllCycles } from '@atomic-state-canvas/asc-viewer-libs';
 import { CSS2DObject, CSS2DRenderer } from 'three/addons/renderers/CSS2DRenderer.js';
+import {
+  generateBidirectionalLinkCountBetweenNodesMap,
+  getDirectionIndependentEdgeId,
+  TViewDimension
+} from './utils';
+import ForceGraph2D, { ForceGraphMethods as ForceGraphMethods2D } from 'react-force-graph-2d';
+
+interface CanvasProps {
+  type: TViewDimension;
+}
 
 type TNodeExtra = { color: string; label: string };
 type TLinkExtra = { color: string };
@@ -22,14 +32,24 @@ const NODE_COLOR_ERROR = 'red';
 const EDGE_COLOR_VALID = 'mediumturquoise';
 const EDGE_COLOR_ERROR = 'orange';
 
-export const Canvas = () => {
+const CURVATURE_FACTOR = 0.2;
+
+export const Canvas = (props: CanvasProps) => {
+  const { type } = props;
   const ascEntry = useAtomValue(currentAscEntryAtom);
-  const graphRef =
+  const graph3dRef =
     useRef<ForceGraphMethods<NodeObject<TNodeExtra>, LinkObject<TNodeExtra, TLinkExtra>>>(
       undefined
     );
+  const graph2dRef =
+    useRef<ForceGraphMethods2D<NodeObject<TNodeExtra>, LinkObject<TNodeExtra, TLinkExtra>>>(
+      undefined
+    );
 
-  const graphData = useMemo<GraphData<TNodeExtra, TLinkExtra>>(() => {
+  const { graphData, bidirectionalLinkCountBetweenNodesMap } = useMemo<{
+    graphData: GraphData<TNodeExtra, TLinkExtra>;
+    bidirectionalLinkCountBetweenNodesMap: Map<string, number> | undefined;
+  }>(() => {
     if (ascEntry) {
       const entryNodeId = ascEntry.entryNodeId;
       const edges = ascEntry.reverseEdges;
@@ -72,32 +92,80 @@ export const Canvas = () => {
               : EDGE_COLOR_VALID
         };
       });
-      return { nodes: Array.from(idToNodesMap.values()), links };
+      const bidirectionalLinkCountBetweenNodesMap =
+        generateBidirectionalLinkCountBetweenNodesMap(edges);
+      return {
+        graphData: { nodes: Array.from(idToNodesMap.values()), links },
+        bidirectionalLinkCountBetweenNodesMap
+      };
     }
-    return { nodes: [], links: [] };
+    return {
+      graphData: { nodes: [], links: [] },
+      bidirectionalLinkCountBetweenNodesMap: undefined
+    };
   }, [ascEntry]);
 
-  return (
+  return type === '2D' ? (
+    <ForceGraph2D
+      ref={graph2dRef}
+      graphData={graphData}
+      nodeLabel={'label'}
+      nodeCanvasObjectMode={() => 'after'}
+      nodeCanvasObject={(node: NodeObject<TNodeExtra>, ctx, globalScale) => {
+        const { label, x = 0, y = 0 } = node;
+        const fontSize = 12 / globalScale;
+        ctx.font = `${fontSize}px Sans-Serif`;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillStyle = 'black';
+        ctx.fillText(label, x, y + 6);
+      }}
+      linkWidth={2}
+      linkDirectionalArrowLength={5}
+      linkDirectionalArrowRelPos={1}
+      linkCurvature={(link: LinkObject<TLinkExtra>) => {
+        const { source, target } = link;
+        if (typeof source !== 'object' || typeof target !== 'object') {
+          return 0;
+        }
+        const edgeId = getDirectionIndependentEdgeId({
+          source: String(source.id ?? ''),
+          target: String(target.id ?? '')
+        });
+        const curvature = Math.max(
+          (bidirectionalLinkCountBetweenNodesMap?.get(edgeId) ?? 0) - 1,
+          0
+        );
+        return curvature * CURVATURE_FACTOR;
+      }}
+      cooldownTicks={100}
+      onEngineStop={() => graph2dRef.current?.zoomToFit(500, 100)}
+    />
+  ) : (
     <ForceGraph3D
-      ref={graphRef}
+      ref={graph3dRef}
       extraRenderers={extraRenderers}
       graphData={graphData}
       linkDirectionalArrowLength={3.5}
       linkDirectionalArrowRelPos={1}
       linkCurvature={0.25}
+      linkOpacity={0.5}
       linkWidth={1}
       nodeColor={'color'}
       nodeLabel={'label'}
       nodeThreeObject={(node: NodeObject<TNodeExtra>) => {
         const nodeEl = document.createElement('div');
         nodeEl.textContent = node.label;
-        nodeEl.style.color = node.color;
+        nodeEl.style.color = 'black';
+        nodeEl.style.fontFamily = 'Sans-Serif';
         nodeEl.style.paddingLeft = '120px';
         return new CSS2DObject(nodeEl);
       }}
       nodeThreeObjectExtend={true}
       linkColor={'color'}
-      backgroundColor="#e5e7eb"
+      backgroundColor="#f8fafc"
+      cooldownTicks={100}
+      onEngineStop={() => graph3dRef.current?.zoomToFit(500, 100)}
     />
   );
 };
